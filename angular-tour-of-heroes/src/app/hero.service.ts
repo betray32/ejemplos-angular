1106ad11d4d6d8b450b386db81fadd04e3f03254
Camilo <contreras.camilo32@gmail.com>
2020-07-11 20:50:50 -0400

    avance en mensajes, tutorial de servicio

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './heroes/hero';
 import { HEROES } from './heroes/mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
-
   /**
    * Metodos
    */
-  getHeroes(): Hero[] {
-    return HEROES;
+  constructor(private messageService: MessageService) { 
+
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,13 +1,6 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  Hero
-} from './hero';
-import {
-  HEROES
-} from './mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -19,12 +12,28 @@ export class HeroesComponent implements OnInit {
   /**
    * variables
    */
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {}
+  /**
+   * Add a private heroService parameter of type HeroService to the constructor.
+   * 
+   * @param heroService 
+   */
+  constructor(private heroService: HeroService) {
 
-  ngOnInit(): void {}
+  }
+
+  /**
+   * Call it in ngOnInit()
+   * 
+   * While you could call getHeroes() in the constructor, that's not the best practice.
+   * Reserve the constructor for simple initialization such as wiring constructor parameters to properties. The constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
+   * Instead, call getHeroes() inside the ngOnInit lifecycle hook and let Angular call ngOnInit() at an appropriate time after constructing a HeroesComponent instance.
+   */
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   /**
    * Metodos
@@ -32,6 +41,14 @@ export class HeroesComponent implements OnInit {
    */
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  /**
+   * Create a method to retrieve the heroes from the service.
+   */
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
