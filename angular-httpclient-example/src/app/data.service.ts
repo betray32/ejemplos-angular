8f7e16e18f856c00a39ac1b33c883737590b99d5
Camilo <contreras.camilo32@gmail.com>
2020-08-10 18:13:15 -0400

    terminado ejemplo de paginador y http data

--- angular-httpclient-example/src/app/data.service.ts
+++ angular-httpclient-example/src/app/data.service.ts
@@ -1,23 +1,99 @@
 import { Injectable } from '@angular/core';
 
 /** Llamadas HTTP a servicios */
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams  } from '@angular/common/http';
 
 /** Manejo de errores */
-import {  throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { retry, catchError, tap  } from 'rxjs/operators';
+
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  /************************
+   * PAGINACION
+   */
+  public first: string = "";
+  public prev: string = "";
+  public next: string = "";
+  public last: string = "";
+  /************************/
+
   private REST_API_SERVER = "http://localhost:3000/products";
 
   constructor(private httpClient: HttpClient) { }
 
+  /**
+   * define the parseLinkHeader() method which parses the Link 
+   * header and populate the previous variables accordingly
+   * @param header 
+   */
+  parseLinkHeader(header) {
+    if (header.length == 0) {
+      return;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach(p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+
+    this.first = links["first"];
+    this.last = links["last"];
+    this.prev = links["prev"];
+    this.next = links["next"];
+  }
+
+  /**
+   * You can use the RxJS retry() operator by piping it (using the pipe() method) 
+   * onto the Observable returned from the HttpClient method before the error handler.
+   * 
+   * This will retry sending the failed HTTP request three times.
+   */
   public sendGetRequest(){
-    return this.httpClient.get(this.REST_API_SERVER);
+
+    /**
+     * We added the observe option with the response value in the 
+     * options parameter of the get() method so we can have the full
+     * HTTP response with headers. Next, we use the RxJS tap() operator 
+     * for parsing the Link header before returning the final Observable.
+     */
+    return this.httpClient.get < Product[] > (this.REST_API_SERVER, {
+      params: new HttpParams({
+        fromString: "_page=1&_limit=3"
+      }),
+      observe: "response"
+    }).pipe(retry(3), catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader(res.headers.get('Link'));
+
+    }));
+    
+  }
+
+  /**
+   * This method is similar to sendGetRequest() except that it takes 
+   * the URL to which we need to send an HTTP GET request.
+   * 
+   * @param url 
+   */
+  public sendGetRequestToUrl(url: string) {
+    return this.httpClient.get < Product[] > (url, {
+      observe: "response"
+    }).pipe(retry(3), catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader(res.headers.get('Link'));
+
+    }));
   }
 
   handleError(error: HttpErrorResponse) {
@@ -32,4 +108,5 @@ export class DataService {
     window.alert(errorMessage);
     return throwError(errorMessage);
   }
+
 }
--- angular-httpclient-example/src/app/home/home.component.ts
+++ angular-httpclient-example/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../data.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +12,81 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
 
-  products = [];
+  products: Product[] = [];
+
+  /**
+   * Next, we created an instance of Subject which can emit boolean values 
+   * (the type of the value doesn't really matter in this example) that will 
+   * be used as the notifier of the takeUntil() operator.
+   */
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
 
-    this.dataService.sendGetRequest().subscribe((data: any[])=>{
-      console.log(data);
-      this.products = data;
-    })  
+    /**
+     * There is also a better way to unsubscribe from or complete Observables by using the takeUntil() operator.
+     * 
+     * The takeUntil() operator emits the values emitted by the source 
+     * Observable until a notifier Observable emits a value.
+     */
+     this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+
   }
 
+  /**
+   * When Angular destroys a component it calls the ngOnDestroy() 
+   * lifecycle method which, in our case, calls the next() method 
+   * to emit a value so RxJS completes all subscribed Observables.
+   */
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
+  }
+
+
+  /*************************
+   * Metodos de paginacion
+   */
+  public firstPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.first).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]> ) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+  public previousPage() {
+
+    if (this.dataService.prev !== undefined && this.dataService.prev !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.prev).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]> ) => {
+        console.log(res);
+        this.products = res.body;
+      })
+    }
+
+  }
+  public nextPage() {
+    if (this.dataService.next !== undefined && this.dataService.next !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.next).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]> ) => {
+        console.log(res);
+        this.products = res.body;
+      })
+    }
+  }
+  public lastPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.last).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]> ) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+  /***********************/
 
 }
