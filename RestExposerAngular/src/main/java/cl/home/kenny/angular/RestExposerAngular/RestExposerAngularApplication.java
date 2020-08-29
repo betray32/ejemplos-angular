5bb6227ede08f7a1a4553c73d241d5fa6cbfb8f0
Camilo <contreras.camilo32@gmail.com>
2020-08-29 12:28:57 -0400

    migrado intelliJ

---
+++ RestExposerAngular/src/main/java/cl/home/kenny/angular/RestExposerAngular/RestExposerAngularApplication.java
@@ -0,0 +1,13 @@
+package cl.home.kenny.angular.RestExposerAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExposerAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestExposerAngularApplication.class, args);
+	}
+
+}
---
+++ RestExposerAngular/src/test/java/cl/home/kenny/angular/RestExposerAngular/RestExposerAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.home.kenny.angular.RestExposerAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExposerAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
